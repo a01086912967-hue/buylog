f7611252753197d8ed731b123d2fdf60061e129f
a01086912967-hue <a01086912967@gmail.com>
2026-09-04 21:32:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ const client = new Client({
 });
 
 const TOKEN = process.env.TOKEN;
-const PURCHASE_LOG_CHANNEL_ID = process.env.PURCHASE_LOG_CHANNEL_ID;
+const PURCHASE_LOG_CHANNEL_ID = '1457384179535712473'; // 로그 전송 채널 고정
 
-// 봇이 켜질 때 슬래시 명령어(/지급완료)를 디스코드에 자동으로 등록합니다.
 client.once('ready', async () => {
-    console.log('봇 준비 완료! (로그 전송 모드)');
+    console.log('봇 준비 완료!');
 
     const commands = [
         new SlashCommandBuilder()
@@ -33,7 +32,7 @@ client.once('ready', async () => {
                     .setRequired(true))
             .addUserOption(option => 
                 option.setName('구매자')
-                    .setDescription('구매한 유저')
+                    .setDescription('구매한 유저 (미입력 시 본인)')
                     .setRequired(false))
             .addUserOption(option => 
                 option.setName('판매자')
@@ -44,7 +43,6 @@ client.once('ready', async () => {
     const rest = new REST({ version: '10' }).setToken(TOKEN);
 
     try {
-        console.log('슬래시 명령어 등록 중...');
         await rest.put(
             Routes.applicationCommands(client.user.id),
             { body: commands }
@@ -59,37 +57,32 @@ client.on('interactionCreate', async interaction => {
     if (interaction.isChatInputCommand() && interaction.commandName === '지급완료') {
         await interaction.reply({ content: '처리를 시작합니다.', ephemeral: true });
 
-        // 1. 수동 입력 옵션값 가져오기
         const itemName = interaction.options.getString('상품');
         const itemQty = interaction.options.getString('수량');
         const amount = interaction.options.getString('금액');
         const buyer = interaction.options.getUser('구매자') || interaction.user;
         const seller = interaction.options.getUser('판매자') || interaction.user;
 
-        // 2. 로그 채널 전송
+        // 1. 지정된 로그 채널 전송
         try {
             const logChannel = await client.channels.fetch(PURCHASE_LOG_CHANNEL_ID);
             if (logChannel) {
                 const logEmbed = new EmbedBuilder()
                     .setColor(0xFFD1DC)
-                    .setDescription(`°.✩┈┈∘*┈˃̶ ୨<:star_IDS:1523988845735972874>୧˂̶┈*∘┈┈✩.°\n\n${buyer} 님, **${itemName} x ${itemQty}** 구매 감사합니다 .ᐟ.ᐟ\n\n-# 사용된 금액 : ${amount}\n\n-# 해당 관리 판매자: ${seller}\n\n°.✩┈┈∘*┈˃̶ ୨<:star_IDS:1523988845735972874>୧˂̶┈*∘┈┈✩.°\n࣪𓏲ּ ᥫ᭡ ₊ 𝑻𝒉𝒂𝒏𝒌 𝒚𝒐𝒖 ⊹ ˑ ִֶ 𓂃`)
+                    .setDescription(`°.✩┈┈∘┈˃̶ ୨<a:Pinkheartgif:1545408138377695352> ୧˂̶┈∘┈┈✩.°\n\n${buyer}, ${itemName} (${itemQty}개) 구매 감사합니다 .ᐟ.ᐟ\n\n사용된 금액 : ${amount}\n\n해당 관리 판매자: ${seller}\n\n°.✩┈┈∘┈˃̶ ୨<a:Pinkheartgif:1545408138377695352> ୧˂̶┈∘┈┈✩.°\n࣪𓏲ּ ᥫ᭡ ₊ 𝑻𝒉𝒂𝒏𝒌 𝒚𝒐𝒖 ⊹ ˑ ִֶ 𓂃`)
                     .setImage('https://i.imgur.com/jokl6LQ.gif');
                 await logChannel.send({ embeds: [logEmbed] });
-            } else {
-                console.error("로그 채널을 찾을 수 없습니다.");
             }
         } catch (error) {
             console.error("로그 채널 전송 오류:", error);
         }
 
-        // 3. 현재 티켓 채널에 메시지 + 임베드 전송
-        const ticketMessage = `${buyer}\n### 지급완료 <a:purple_check:1482280715147018371>\n- 확인 후 <#1457384179535712473> 작성 필수`;
+        // 2. 현재 티켓 채널에 임베드만 전송 (구매자 멘션 포함)
         const ticketEmbed = new EmbedBuilder()
             .setColor(0xFFD1DC)
-            .setDescription(`아이템이 정상적으로 지급되었어요. <a:veryheart:1479957265871143104>\nhttps://discord.com/channels/1456729030459134115/1457384179535712473 작성은 필수입니다`);
+            .setDescription(`${buyer}님, 아이템이 정상적으로 지급되었어요. <a:veryheart:1479957265871143104>\nhttps://discord.com/channels/1456729030459134115/1457384179535712473 작성은 필수입니다`);
 
         await interaction.channel.send({
-            content: ticketMessage,
             embeds: [ticketEmbed]
         });
     }
